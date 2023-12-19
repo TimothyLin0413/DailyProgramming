a7427ef06bd04dec1d6525e1f95be7974630dd0f
TimothyLin0413 <73677800+TimothyLin0413@users.noreply.github.com>
2023-12-18 22:23:46 -0800

    Update MySolution.java

--- MySolution.java
+++ MySolution.java
@@ -1,1 +1,14 @@
 
+class Solution {
+    // This function remove the duplicate numbers in a non-decreasing array while keeping the positions. 
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
