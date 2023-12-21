f2a84cbbe921856d71ab9fbe09686c07c199e750
TimothyLin0413 <73677800+TimothyLin0413@users.noreply.github.com>
2023-12-21 11:20:31 -0800

    Update and rename MySolution.java to TimosProgrammingLibrary.java

--- TimosProgrammingLibrary.java
+++ TimosProgrammingLibrary.java
@@ -1,6 +1,6 @@
 
-class Solution {
-    // This function remove the duplicate numbers in a non-decreasing array while keeping the positions. 
+class ProgrammingLibrary {
+    // 12/20/23 This function removes the duplicate numbers in a non-decreasing array while keeping the positions. 
     public int removeDuplicates(int[] nums) {
         int j = 1;
         for (int i = 1; i < nums.length; i++) {
