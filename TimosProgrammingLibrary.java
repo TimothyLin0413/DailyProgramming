e7b2c13b2c1f442d67ef0c50674e3fe9d5a621c1
TimothyLin0413 <73677800+TimothyLin0413@users.noreply.github.com>
2023-12-21 21:11:58 -0800

    12/21/23

--- TimosProgrammingLibrary.java
+++ TimosProgrammingLibrary.java
@@ -11,4 +11,64 @@ class ProgrammingLibrary {
         }
         return j;
     }
+
+    // 12/21/23
+    // Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        // word is bigger than grid size 
+        if (m*n < word.length())
+            return false;
+            
+        char[] wrd = word.toCharArray();
+        int[] boardf = new int[128];
+        // get the count of character 
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                ++boardf[board[i][j]];
+            }
+        }
+        // if count of the letter is used more than the count in the grid return false
+        for (char ch : wrd)
+        {
+            if (--boardf[ch] < 0)
+            {
+                return false;
+            }
+        }
+        // go through the grid and see the letters are adjacent to each other and able to form the word
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (wrd[0] == board[i][j]
+                    && found(board, i, j, wrd, new boolean[m][n], 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private static final int[] dirs = {0, -1, 0, 1, 0};
+    private boolean found(char[][] board, int row, int col, char[] word,
+                        boolean[][] visited, int index)
+    {
+        // set the position of the found letter to true and recursive call the function until all letters are found or return false otherwise
+        if (index == word.length)
+            return true;
+        if (row < 0 || col < 0 || row == board.length || col == board[0].length
+            || board[row][col] != word[index] || visited[row][col])
+            return false;
+        visited[row][col] = true;
+        for (int i = 0; i < 4; ++i)
+        {
+            if (found(board, row + dirs[i], col + dirs[i + 1],
+                word, visited, index + 1))
+                return true;
+        }
+        visited[row][col] = false;
+        return false;
+    }
 }
