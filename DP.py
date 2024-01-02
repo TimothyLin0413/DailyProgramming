7a5ea870aa7b775e7218dac03c08ca2fba2ccd5d
TimothyLin0413 <73677800+TimothyLin0413@users.noreply.github.com>
2024-01-01 16:49:04 -0800

    Update DP.py with obstacles in the grid

--- DP.py
+++ DP.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 12/30/2023
     '''
     There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
     Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
@@ -13,3 +14,24 @@ class Solution:
             curr_row, prev_row = prev_row, curr_row
         
         return prev_row[-1]
+
+    # 12/31/2023
+    # With obstacles
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if not obstacleGrid or not obstacleGrid[0] or obstacleGrid[0][0] == 1:
+            return 0
+        
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        
+        previous = [0] * n
+        current = [0] * n
+        previous[0] = 1
+        
+        for i in range(m):
+            current[0] = 0 if obstacleGrid[i][0] == 1 else previous[0]
+            for j in range(1, n):
+                current[j] = 0 if obstacleGrid[i][j] == 1 else current[j-1] + previous[j]
+            previous[:] = current
+        
+        return previous[n-1]
+    
