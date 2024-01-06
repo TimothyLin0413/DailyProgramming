1450145505e85d606d1ce25974a18c3f7c92c0dc
TimothyLin0413 <73677800+TimothyLin0413@users.noreply.github.com>
2024-01-05 22:42:08 -0800

    1/5/24

--- TimosTreeLibrary.java
+++ TimosTreeLibrary.java
@@ -27,4 +27,30 @@ class TreeLibrary {
         // Recursively check if the left and right subtrees are identical
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
+
+    // Check if a given tree is balanced
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return isBalancedTreeNode(root) != -1;
+    }
+
+    public int isBalancedTreeNode(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = isBalancedTreeNode(root.left);
+        int right = isBalancedTreeNode(root.right);
+
+        if (left == -1 || right == -1) {
+            return -1;
+        }
+
+        int diff = Math.abs(left - right);
+        if(diff > 1) {
+            return -1;
+        }
+        
+        return 1 + Math.max(left, right);
+    }
 }
